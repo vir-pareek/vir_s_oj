b86f6a215affd157e8fbce6cc0073df4e7c633ce
vir-pareek <vir24112004@gmail.com>
2025-07-26 01:16:04 +0530

    updated index.js

--- authentication/backend/index.js
+++ authentication/backend/index.js
@@ -15,10 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//...
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true, //allows cookies to be sent with requests
+    origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : "http://localhost:5173",
+    credentials: true,
 }));
+//...
 
 app.use(express.json()); //allows us to parse incoming req with json payloads or allows parsing incoming requests : req.body
 app.use(cookieParser()); //allows us to parse cookies from incoming requests

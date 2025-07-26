0d8adbdd66bc385de89fb46bba2e8c43788e3011
vir-pareek <vir24112004@gmail.com>
2025-07-26 09:58:56 +0530

    trying to deploy on vercel

--- authentication/backend/index.js
+++ authentication/backend/index.js
@@ -33,7 +33,10 @@ const startServer = async () => {
         const __dirname = path.dirname(__filename);
 
         app.use(cors({
-            origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+            // Vercel automatically sets VERCEL_URL. Use it for production.
+            origin: process.env.NODE_ENV === "production"
+                ? `https://${process.env.VERCEL_URL}`
+                : "http://localhost:5173",
             credentials: true,
         }));
         app.use(express.json());

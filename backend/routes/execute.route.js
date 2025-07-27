03c2dece6fb0051a90d2b8a699e864185feb8e51
vir-pareek <vir24112004@gmail.com>
2025-07-27 17:31:02 +0530

    added aws and .env for compiler port

--- backend/routes/execute.route.js
+++ backend/routes/execute.route.js
@@ -1,13 +1,17 @@
 import express from "express";
 import axios from "axios";
 const router = express.Router();
-
+import dotenv from "dotenv";
+dotenv.config();
+const compilerServiceUrl = process.env.COMPILER_SERVICE_URL;
 // POST /api/execute
 router.post("/execute", async (req, res) => {
   const { code, language, input } = req.body;
   try {
     // Forward the request to the compiler service
-    const response = await axios.post("http://localhost:8000/run", {
+    console.log(`Forwarding request to compiler service at ${compilerServiceUrl}`);
+    
+    const response = await axios.post(`${compilerServiceUrl}`, {
       code,
       language,
       input,

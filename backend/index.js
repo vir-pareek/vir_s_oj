b22d1a69390ddc701651d1425d4b632d24df3d6b
vir-pareek <vir24112004@gmail.com>
2025-07-27 18:21:39 +0530

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true, //allows cookies to be sent with requests
 }));
 
